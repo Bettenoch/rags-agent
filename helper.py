fc2dbfcdc5c5336211c29695855ff965ec587cd2
Bettenoch <bettenoch254@gmail.com>
2024-06-18 12:09:42 +0300

    create helper function to store api key

---
+++ helper.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+import openai
+
+def load_env():
+    _ = load_dotenv(find_dotenv())
+
+def get_openai_api_key():
+    load_env()
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    return openai_api_key
+
+openai_api_key = get_openai_api_key()
+client = openai.OpenAI(api_key=openai_api_key)
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    return response.choices[0].message.content
